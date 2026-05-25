c0af3bad915f1cd803721601dde389bff0732b95
crueter <crueter@eden-emu.dev>
2026-05-24 23:17:57 -0400

    nasm
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ nasm.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+case "$PROCESSOR_ARCHITECTURE" in
+	AMD64) ;;
+	*) exit 0 ;;
+esac
+
+
+: "${VERSION:=3.01}"
+artifact="nasm-$VERSION-win64.zip"
+dir="nasm-$VERSION"
+url="https://www.nasm.us/pub/nasm/releasebuilds/$VERSION/win64/$artifact"
+
+curl -sfLO "$url"
+unzip "$artifact"
+
+out="/c/hostedtoolcache/windows/nasm"
+mkdir -p "$out"
+mv "$dir"/nasm.exe "$out"
+rm -rf "$dir"
+
+out_win=$(cygpath -w "$out")
+echo "$out_win" >> "$GITHUB_PATH"
