946770a1ab159a9fb42a5b5df221ee0dae003671
crueter <crueter@eden-emu.dev>
2026-05-24 23:09:33 -0400

    Oh
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- zstd.sh
+++ zstd.sh
@@ -10,7 +10,7 @@ repo=crueter-ci/zstd
 : "${ZSTD_VERSION:=1.5.7}"
 artifact="zstd-$arch.exe"
 
-curl -sfL "https://github.com/$repo/releases/download/v$PKGCONF_VERSION/$artifact" -o zstd.exe
+curl -sfL "https://github.com/$repo/releases/download/v$ZSTD_VERSION/$artifact" -o zstd.exe
 
 out="/c/hostedtoolcache/windows/zstd"
 mkdir -p "$out"
