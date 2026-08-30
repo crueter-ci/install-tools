d53510204b2e144b02824e1b7d64918f30bbfed7
crueter <crueter@eden-emu.dev>
2026-08-30 00:34:19 -0400

    add jom
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ jom.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+case "$(uname -a)" in
+	*ARM64*) arch=arm64 ;;
+	*) arch=amd64 ;;
+esac
+
+repo=crueter-ci/jom
+: "${JOM_VERSION:=1.1.7}"
+artifact="jom-$arch.exe"
+
+curl -sfL "https://github.com/$repo/releases/download/v$JOM_VERSION/$artifact" -o jom.exe
+
+out="/c/hostedtoolcache/windows/jom"
+mkdir -p "$out"
+mv jom.exe "$out"
+
+out_win=$(cygpath -w "$out")
+echo "$out_win" >> "$GITHUB_PATH"
